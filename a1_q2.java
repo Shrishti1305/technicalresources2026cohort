925a95debaf7d005f99096494f2758777ef2b78d
Shrishti1305 <122806572+Shrishti1305@users.noreply.github.com>
2023-01-19 23:56:50 +0530

    Submitting q2 of assignment 1

---
+++ a1_q2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // input - don't change this code
+    Scanner scn = new Scanner(System.in);
+    int marks = scn.nextInt();
+    // input - don't change this code
+
+   int marks = 10 ;
+   
+   if (marks>90){
+   System.out.println("Excellent");
+       
+   } else if (marks>80){
+       System.out.printlm("good");
+   }else if (marks>70){
+       System.out.println("fair");
+   }else if (marks>60){
+       System.out.println("meets expectation");
+   }else{
+       System.out.println("below par");
+   }
+   } 
+       
+       
+   }
+   
+   
+   
+   
+   
+   
+   
+   
+   
+  }
+}
